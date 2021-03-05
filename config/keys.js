dab5e767653f28d2633b92090ae176bc2a612c8d
Vishnu Prasad <vishnu337247@gmail.com>
2021-03-05 11:18:39 +0530

    Password Hashed

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://VP:Appu@1996@cluster0.5kq62.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://UserMain:deetgm6@JgtRAF5@cluster0.5kq62.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    //db = mongojs('mongodb://username:password@ds31341.mongolab.com:32132/mydb', ["mycollection"], {authMechanism: 'ScramSHA1'});
 }
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express')
 
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+//User Model
+const User = require('../models/User');
 
 //Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -39,7 +42,45 @@ router.post('/register',(req,res) => {
         })
 
     }else {
-        res.send('pass')
+       //Validation Passed
+       User.findOne({email: email})
+       .then(user => {
+           if(user){
+               //User Exists
+               errors.push({msg: 'Email is already registered'})
+               res.render("register",{
+                errors,
+                name,
+                email,
+                password,
+                password2
+            });
+
+           }else {
+               const newUser = new User({
+                   name,
+                   email,
+                   password
+               });
+              //Hash Password
+              bcrypt.genSalt(10, (err, salt) => 
+              bcrypt.hash(newUser.password,salt,(err,hash) =>{
+                  if(err) throw err;
+
+                 //Set password to hashed
+                 newUser.password = hash
+                 //Save user
+                 newUser.save()
+                 .then(user => {
+                     res.redirect('/users/login');
+                 })
+                 .catch(err => console.log(err));
+
+
+              }))
+           }
+       });
+
     }
     //console.log(req.body)
     //res.send('hello');
